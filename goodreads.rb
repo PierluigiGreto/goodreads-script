224915a3c1c1f02b3546ef6048d8aba9e6b4a020
kaarot_kalel_90 <gretopierluigi@gmail.com>
2016-12-23 14:26:43 +0100

    Fix

--- goodreads.rb
+++ goodreads.rb
@@ -6,28 +6,22 @@ config["config"].each { |key, value| instance_variable_set("@#{key}", value) }
 client = Goodreads::Client.new(api_key: @api_key, api_secret: @api_secret)
 shelf =  client.shelf(@user_id, @shelf)
 books = shelf.books
-for book in books
-	puts book.book.title
-	puts book.book.link
-	puts book.book.image_url
-	puts book
-	  	for author in book.book.authors
-		  puts author[1].name
-		  break
-		end
-	  break
-end
-
 
 file = open("README.md", 'w')
-
 
 file.write("###### My Readings\n")
 
 
-
-
-
+for book in books
+	title = book.book.title
+	link =  book.book.link
+	image = book.book.image_url
+	file.write("- ![My image](#{image})[#{title}](#{link}) - ")
+	  	for author in book.book.authors
+		  file.write(author[1].name)
+		end
+	file.write("\n")
+end
 
 file.write("___\n")
 file.write("I used the script [goodreads-script](https://github.com/PierluigiGreto/goodreads-script)\n")
